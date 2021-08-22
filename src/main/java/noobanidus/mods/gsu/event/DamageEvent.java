57ced52056b753e305d0f51e25a889a420feb0e3
Jon McManus <due@wxwhatever.com>
2021-08-22 11:29:34 +1000

    Cactus shield.
    Extend all shield events to all events.
    
    This should hopefully allow for things to be lava immune, etc.

--- src/main/java/noobanidus/mods/gsu/event/DamageEvent.java
+++ src/main/java/noobanidus/mods/gsu/event/DamageEvent.java
@@ -3,39 +3,46 @@ package noobanidus.mods.gsu.event;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.potion.Effect;
-import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.vector.Vector3d;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.living.LivingKnockBackEvent;
+import net.minecraftforge.event.entity.living.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import noobanidus.mods.gsu.GSU;
 import noobanidus.mods.gsu.init.ModEffects;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
 @Mod.EventBusSubscriber(modid = GSU.MODID)
 public class DamageEvent {
-  @SubscribeEvent
-  public static void onDamage(LivingAttackEvent event) {
-    Set<Effect> effects = new HashSet<>();
-    for (EffectInstance instance : event.getEntityLiving().getActivePotionEffects()) {
-      effects.add(instance.getPotion());
-    }
-    if (effects.contains(ModEffects.IMMORTAL.get()) || effects.contains(ModEffects.IMMORTAL_DYING.get())) {
-      if (event.getSource() != DamageSource.OUT_OF_WORLD) {
+  private static void livingEvent(LivingEvent event, DamageSource source) {
+    LivingEntity entity = event.getEntityLiving();
+    if (source != DamageSource.OUT_OF_WORLD) {
+      if (entity.getActivePotionEffect(ModEffects.IMMORTAL.get()) != null || entity.getActivePotionEffect(ModEffects.IMMORTAL_DYING.get()) != null) {
+        event.setCanceled(true);
+        return;
+      }
+      if (source == DamageSource.CACTUS && entity.getActivePotionEffect(ModEffects.CACTUS_SHIELD.get()) != null) {
         event.setCanceled(true);
       }
     }
   }
 
   @SubscribeEvent
-  public static void onKnockup (LivingKnockBackEvent event) {
+  public static void onAttack(LivingAttackEvent event) {
+    livingEvent(event, event.getSource());
+  }
+
+  @SubscribeEvent
+  public static void onHurt(LivingHurtEvent event) {
+    livingEvent(event, event.getSource());
+  }
+
+  @SubscribeEvent
+  public static void onDamage(LivingDamageEvent event) {
+    livingEvent(event, event.getSource());
+  }
+
+  @SubscribeEvent
+  public static void onKnockup(LivingKnockBackEvent event) {
     LivingEntity living = event.getEntityLiving();
     if (living.getRevengeTarget() instanceof MobEntity) {
       MobEntity attacker = (MobEntity) living.getRevengeTarget();
@@ -43,7 +50,7 @@ public class DamageEvent {
         event.setCanceled(true);
         float strength = event.getStrength();
         double ratioX = event.getRatioX();
-        strength = (float)((double)strength * (1.0D - living.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE)));
+        strength = (float) ((double) strength * (1.0D - living.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE)));
         if (!(strength <= 0.0F)) {
           living.isAirBorne = true;
           Vector3d vector3d = living.getMotion();
