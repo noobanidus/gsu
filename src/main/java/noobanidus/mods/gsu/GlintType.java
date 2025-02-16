5e0fc9ab137067f14e7dc832a9d172b969f0a562
Jon <due@wxwhatever.com>
2025-02-16 12:09:55 +1000

    Add "super bright" variants.

--- src/main/java/noobanidus/mods/gsu/GlintType.java
+++ src/main/java/noobanidus/mods/gsu/GlintType.java
@@ -10,7 +10,7 @@ import net.minecraft.util.StringRepresentable;
 import java.util.function.IntFunction;
 
 public enum GlintType implements StringRepresentable {
-  DEFAULT("default"), BRIGHT_DEFAULT("bright_default"), GOLD("gold"), BRIGHT_GOLD("bright_gold"), LIME("lime"), BRIGHT_LIME("bright_lime"), CYAN("cyan"), BRIGHT_CYAN("bright_cyan");
+  DEFAULT("default"), BRIGHT_DEFAULT("bright_default"), GOLD("gold"), BRIGHT_GOLD("bright_gold"), LIME("lime"), BRIGHT_LIME("bright_lime"), CYAN("cyan"), BRIGHT_CYAN("bright_cyan"), SUPER_BRIGHT_DEFAULT("super_bright_default"), SUPER_BRIGHT_GOLD("super_bright_gold"), SUPER_BRIGHT_LIME("super_bright_lime"), SUPER_BRIGHT_CYAN("super_bright_cyan");
 
   public static final IntFunction<GlintType> BY_ID = ByIdMap.continuous(GlintType::ordinal, GlintType.values(), ByIdMap.OutOfBoundsStrategy.ZERO);
   public static final Codec<GlintType> CODEC = StringRepresentable.fromEnum(GlintType::values);
--- src/main/java/noobanidus/mods/gsu/client/CustomRenderType.java
+++ src/main/java/noobanidus/mods/gsu/client/CustomRenderType.java
@@ -11,12 +11,16 @@ import noobanidus.mods.gsu.GlintType;
 public class CustomRenderType extends RenderType {
   public static ResourceLocation DEFAULT_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/default_entity_glint.png");
   public static ResourceLocation BRIGHT_DEFAULT_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/bright_default_entity_glint.png");
+  public static ResourceLocation SUPER_BRIGHT_DEFAULT_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/super_bright_default_entity_glint.png");
   public static ResourceLocation GOLD_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/gold_entity_glint.png");
   public static ResourceLocation BRIGHT_GOLD_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/bright_gold_entity_glint.png");
+  public static ResourceLocation SUPER_BRIGHT_GOLD_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/super_bright_gold_entity_glint.png");
   public static ResourceLocation LIME_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/lime_entity_glint.png");
   public static ResourceLocation BRIGHT_LIME_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/bright_lime_entity_glint.png");
+  public static ResourceLocation SUPER_BRIGHT_LIME_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/super_bright_lime_entity_glint.png");
   public static ResourceLocation CYAN_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/cyan_entity_glint.png");
   public static ResourceLocation BRIGHT_CYAN_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/bright_cyan_entity_glint.png");
+  public static ResourceLocation SUPER_BRIGHT_CYAN_ENTITY_GLINT = ResourceLocation.fromNamespaceAndPath("gsu", "textures/misc/glint/super_bright_entity_glint.png");
 
   private static final RenderStateShard.LayeringStateShard CUSTOM_POLYGON_OFFSET_LAYERING = new RenderStateShard.LayeringStateShard(
       "polygon_offset_layering", () -> {
@@ -64,6 +68,24 @@ public class CustomRenderType extends RenderType {
           .setLayeringState(CUSTOM_POLYGON_OFFSET_LAYERING)
           .createCompositeState(false)
   );
+  public static final RenderType SUPER_BRIGHT_DEFAULT_GLINT = create(
+      "super_bright_default_glint",
+      DefaultVertexFormat.POSITION_TEX,
+      VertexFormat.Mode.QUADS,
+      1536,
+      false,
+      false,
+      RenderType.CompositeState.builder()
+          .setShaderState(RENDERTYPE_ENTITY_GLINT_SHADER)
+          .setTextureState(new RenderStateShard.TextureStateShard(SUPER_BRIGHT_DEFAULT_ENTITY_GLINT, true, false))
+          .setWriteMaskState(COLOR_WRITE)
+          .setCullState(NO_CULL)
+          .setDepthTestState(LEQUAL_DEPTH_TEST)
+          .setTransparencyState(GLINT_TRANSPARENCY)
+          .setTexturingState(GLINT_TEXTURING)
+          .setLayeringState(CUSTOM_POLYGON_OFFSET_LAYERING)
+          .createCompositeState(false)
+  );
 
   public static final RenderType GOLD_GLINT = create(
       "gold_glint",
@@ -94,6 +116,24 @@ public class CustomRenderType extends RenderType {
       RenderType.CompositeState.builder()
           .setShaderState(RENDERTYPE_ENTITY_GLINT_SHADER)
           .setTextureState(new RenderStateShard.TextureStateShard(BRIGHT_GOLD_ENTITY_GLINT, true, false))
+          .setWriteMaskState(COLOR_WRITE)
+          .setCullState(NO_CULL)
+          .setDepthTestState(LEQUAL_DEPTH_TEST)
+          .setTransparencyState(GLINT_TRANSPARENCY)
+          .setTexturingState(GLINT_TEXTURING)
+          .setLayeringState(CUSTOM_POLYGON_OFFSET_LAYERING)
+          .createCompositeState(false)
+  );
+  public static final RenderType SUPER_BRIGHT_GOLD_GLINT = create(
+      "super_bright_gold_glint",
+      DefaultVertexFormat.POSITION_TEX,
+      VertexFormat.Mode.QUADS,
+      1536,
+      false,
+      false,
+      RenderType.CompositeState.builder()
+          .setShaderState(RENDERTYPE_ENTITY_GLINT_SHADER)
+          .setTextureState(new RenderStateShard.TextureStateShard(SUPER_BRIGHT_GOLD_ENTITY_GLINT, true, false))
           .setWriteMaskState(COLOR_WRITE)
           .setCullState(NO_CULL)
           .setDepthTestState(LEQUAL_DEPTH_TEST)
@@ -140,6 +180,25 @@ public class CustomRenderType extends RenderType {
           .setLayeringState(CUSTOM_POLYGON_OFFSET_LAYERING)
           .createCompositeState(false)
   );
+  public static final RenderType SUPER_BRIGHT_CYAN_GLINT = create(
+      "super_bright_cyan_glint",
+      DefaultVertexFormat.POSITION_TEX,
+      VertexFormat.Mode.QUADS,
+      1536,
+      false,
+      false,
+      RenderType.CompositeState.builder()
+          .setShaderState(RENDERTYPE_ENTITY_GLINT_SHADER)
+          .setTextureState(new RenderStateShard.TextureStateShard(SUPER_BRIGHT_CYAN_ENTITY_GLINT, true, false))
+          .setWriteMaskState(COLOR_WRITE)
+          .setCullState(NO_CULL)
+          .setDepthTestState(LEQUAL_DEPTH_TEST)
+          .setTransparencyState(GLINT_TRANSPARENCY)
+          .setTexturingState(GLINT_TEXTURING)
+          .setLayeringState(CUSTOM_POLYGON_OFFSET_LAYERING)
+          .createCompositeState(false)
+  );
+
 
   public static final RenderType LIME_GLINT = create(
       "lime_glint",
@@ -178,9 +237,31 @@ public class CustomRenderType extends RenderType {
           .setLayeringState(CUSTOM_POLYGON_OFFSET_LAYERING)
           .createCompositeState(false)
   );
+  public static final RenderType SUPER_BRIGHT_LIME_GLINT = create(
+      "super_bright_lime_glint",
+      DefaultVertexFormat.POSITION_TEX,
+      VertexFormat.Mode.QUADS,
+      1536,
+      false,
+      false,
+      RenderType.CompositeState.builder()
+          .setShaderState(RENDERTYPE_ENTITY_GLINT_SHADER)
+          .setTextureState(new RenderStateShard.TextureStateShard(SUPER_BRIGHT_LIME_ENTITY_GLINT, true, false))
+          .setWriteMaskState(COLOR_WRITE)
+          .setCullState(NO_CULL)
+          .setDepthTestState(LEQUAL_DEPTH_TEST)
+          .setTransparencyState(GLINT_TRANSPARENCY)
+          .setTexturingState(GLINT_TEXTURING)
+          .setLayeringState(CUSTOM_POLYGON_OFFSET_LAYERING)
+          .createCompositeState(false)
+  );
 
   public static RenderType getByGlint(GlintType glint) {
     return switch (glint) {
+      case GlintType.SUPER_BRIGHT_CYAN -> SUPER_BRIGHT_CYAN_GLINT;
+      case GlintType.SUPER_BRIGHT_LIME -> SUPER_BRIGHT_LIME_GLINT;
+      case GlintType.SUPER_BRIGHT_GOLD -> SUPER_BRIGHT_GOLD_GLINT;
+      case GlintType.SUPER_BRIGHT_DEFAULT -> SUPER_BRIGHT_DEFAULT_GLINT;
       case GlintType.BRIGHT_CYAN -> BRIGHT_CYAN_GLINT;
       case GlintType.CYAN -> CYAN_GLINT;
       case GlintType.BRIGHT_LIME -> BRIGHT_LIME_GLINT;
